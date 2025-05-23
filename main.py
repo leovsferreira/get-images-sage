12d1c2dab877722481cf524dbea4aba908574f8f
lferr10 <leovsferreira@gmail.com>
2025-05-23 13:31:14 -0500

    adding plugin

---
+++ main.py
@@ -0,0 +1,17 @@
+from waggle.plugin import Plugin
+from waggle.data.vision import Camera
+
+
+def main():
+    with Plugin() as plugin:
+        with Camera("bottom_camera") as camera:
+            snapshot = camera.snapshot()
+        
+        timestamp = snapshot.timestamp
+        
+        snapshot.save("snapshot.jpg")
+        plugin.upload_file("snapshot.jpg", timestamp=timestamp)
+
+
+if __name__ == "__main__":
+    main()
