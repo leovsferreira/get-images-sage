c5fcefbc165721eb167b59b973eda4956b90df8c
lferr10 <leovsferreira@gmail.com>
2025-05-27 16:32:58 -0500

    adding image as local file so other plugin can consume

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+import os
 from waggle.plugin import Plugin
 from waggle.data.vision import Camera
 
 
 def main():
+    shared_image_dir = "/app/shared_images"
+    shared_image_path = os.path.join(shared_image_dir, "latest_snapshot.jpg")
+    
+    os.makedirs(shared_image_dir, exist_ok=True)
+    
     with Plugin() as plugin:
         with Camera("bottom_camera") as camera:
             snapshot = camera.snapshot()
         
         timestamp = snapshot.timestamp
-        snapshot.save("snapshot.jpg")
-        plugin.upload_file("snapshot.jpg", timestamp=timestamp)
+        
+        snapshot.save(shared_image_path)
+        
+        plugin.upload_file(shared_image_path, timestamp=timestamp)
 
 
 if __name__ == "__main__":
