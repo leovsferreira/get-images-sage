50dcd02c2df88c16f11bf0238998c56ee2476dfc
lferr10 <leovsferreira@gmail.com>
2025-05-29 11:37:35 -0500

    bottom camera added

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from waggle.data.vision import Camera
 def main():
     with Plugin() as plugin:
         with Camera() as camera:
-            snapshot = camera.snapshot()
+            snapshot = camera.snapshot("bottom_camera")
             timestamp = snapshot.timestamp
             print("snapshot taken")
             snapshot.save("snapshot.jpg")
