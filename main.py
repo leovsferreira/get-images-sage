3ce6bb23b9b063fb856f1a3aff7ca0b2a02d9983
lferr10 <leovsferreira@gmail.com>
2025-05-29 11:22:03 -0500

    trying to force error

--- main.py
+++ main.py
@@ -3,21 +3,12 @@ from waggle.data.vision import Camera
 
 def main():
     with Plugin() as plugin:
-        try:
-            available_cameras = Camera.list_cameras()
-            print(f"Available cameras: {available_cameras}")
-        except Exception as e:
-            print(f"Error listing cameras: {e}")
-        
-        try:
-            with Camera() as camera:
-                snapshot = camera.snapshot()
-                timestamp = snapshot.timestamp
-                print("snapshot taken")
-                snapshot.save("snapshot.jpg")
-                plugin.upload_file("snapshot.jpg", timestamp=timestamp)
-        except Exception as e:
-            print(f"Camera error: {e}")
+        with Camera() as camera:
+            snapshot = camera.snapshot()
+            timestamp = snapshot.timestamp
+            print("snapshot taken")
+            snapshot.save("snapshot.jpg")
+            plugin.upload_file("snapshot.jpg", timestamp=timestamp)
 
 if __name__ == "__main__":
     main()
